4ce591ebc7ab68052651f3eac095872e7effe80e
Alan Carvalho de Assis <acassis@gmail.com>
2025-12-28 15:50:26 -0300

    Send RTK position to BS

--- examples/rover/rover_main.c
+++ examples/rover/rover_main.c
@@ -48,10 +48,147 @@
 #define GGA_BUF_SZ    256
 #define FRAME_SIZE    63
 
+struct __attribute__((packed)) rtk_frame_s
+{
+  uint32_t timestamp;   /* seconds since epoch */
+
+  int32_t  lat;         /* latitude  * 1e7 degrees */
+  int32_t  lon;         /* longitude * 1e7 degrees */
+  int32_t  alt;         /* altitude in millimeters */
+
+  uint16_t velocity;    /* cm/s */
+  uint16_t heading;     /* degrees * 100 */
+
+  uint8_t  fix;         /* 0=no fix, 1=float, 2=fixed */
+  uint16_t id;          /* rover or frame ID */
+};
 
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
+
+static char *nmea_field(char *s, int index)
+{
+  for (int i = 0; i < index; i++)
+    {
+      s = strchr(s, ',');
+      if (!s)
+        return NULL;
+      s++;
+    }
+  return s;
+}
+
+void parse_gga(char *nmea, struct rtk_frame_s *f)
+{
+  char *p;
+
+  /* Latitude */
+  p = nmea_field(nmea, 2);
+  if (p == NULL)
+	  return;
+
+  double lat = atof(p);
+  char ns = *nmea_field(nmea, 3);
+  if (ns == NULL)
+	  return;
+
+  double deg = floor(lat / 100.0);
+  double min = lat - deg * 100.0;
+  double dec = deg + min / 60.0;
+
+  f->lat = (int32_t)(dec * 1e7);
+  if (ns == 'S') f->lat = -f->lat;
+
+  /* Longitude */
+  p = nmea_field(nmea, 4);
+  if (p == NULL)
+	  return;
+
+  double lon = atof(p);
+  char ew = *nmea_field(nmea, 5);
+  if (ew == NULL)
+	  return;
+
+
+  deg = floor(lon / 100.0);
+  min = lon - deg * 100.0;
+  dec = deg + min / 60.0;
+
+  f->lon = (int32_t)(dec * 1e7);
+  if (ew == 'W') f->lon = -f->lon;
+
+  /* Fix */
+
+  p = nmea_field(nmea, 6);
+  if (p == NULL)
+	  return;
+
+  f->fix = (uint8_t)atoi(p);
+
+  /* Altitude */
+  p = nmea_field(nmea, 9);
+  if (p == NULL)
+	  return;
+
+  f->alt = (int32_t)(atof(p) * 1000.0);
+}
+
+static time_t rmc_to_unix(const char *utc, const char *date)
+{
+  struct tm tm = {0};
+
+  /* Time: hhmmss[.ss] */
+  tm.tm_hour = (utc[0] - '0') * 10 + (utc[1] - '0');
+  tm.tm_min  = (utc[2] - '0') * 10 + (utc[3] - '0');
+  tm.tm_sec  = (utc[4] - '0') * 10 + (utc[5] - '0');
+
+  /* Date: ddmmyy */
+  tm.tm_mday = (date[0] - '0') * 10 + (date[1] - '0');
+  tm.tm_mon  = ((date[2] - '0') * 10 + (date[3] - '0')) - 1;
+  tm.tm_year = ((date[4] - '0') * 10 + (date[5] - '0')) + 100;
+
+  /* UTC */
+  return timegm(&tm);
+}
+
+void parse_rmc(char *nmea, struct rtk_frame_s *f)
+{
+  char *p;
+
+  /* Status: A = valid */
+  p = nmea_field(nmea, 2);
+  if (!p || *p != 'A')
+    {
+      f->fix = 0;
+      return;
+    }
+
+  /* Timestamp */
+  char *utc  = nmea_field(nmea, 1);
+  char *date = nmea_field(nmea, 9);
+  if (utc && date)
+    {
+      f->timestamp = (uint32_t)rmc_to_unix(utc, date);
+    }
+
+  /* Speed: knots → m/s */
+  p = nmea_field(nmea, 7);
+  if (p && *p)
+    {
+      double knots = atof(p);
+      double ms = knots * 0.514444;
+      f->velocity = (uint16_t)(ms * 100.0);
+    }
+
+  /* Course → heading (deg * 100) */
+  p = nmea_field(nmea, 8);
+  if (p && *p)
+    {
+      double course = atof(p);
+      f->heading = (uint16_t)(course * 100.0);
+    }
+}
 
 static int serial_read_gga(int fd, char *gga, size_t max)
 {
@@ -78,6 +215,30 @@ static int serial_read_gga(int fd, char *gga, size_t max)
     return 0;
 }
 
+static int serial_read_rmc(int fd, char *rmc, size_t max)
+{
+    static char line[GGA_BUF_SZ];
+    static size_t idx = 0;
+    char c;
+    
+    while (read(fd, &c, 1) == 1) {
+        if (c == '\n') {
+            line[idx] = 0;
+            idx = 0;
+    
+	    if (!strncmp(line, "$GNRMC", 6)) {
+                strncpy(rmc, line, max - 1);
+                rmc[max - 1] = 0;
+                return 1;
+            }
+        } else if (idx < sizeof(line) - 1) {
+            line[idx++] = c;
+        }
+    }
+
+    return 0;
+}
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -85,7 +246,9 @@ static int serial_read_gga(int fd, char *gga, size_t max)
 int main(int argc, FAR char *argv[])
 {
     struct sx127x_read_hdr_s data;
+    struct rtk_frame_s rtk_frame;
     char gga[GGA_BUF_SZ];
+    char rmc[GGA_BUF_SZ];
     int i = 0;
     int ret;
     int s_fd;
@@ -240,6 +403,22 @@ int main(int argc, FAR char *argv[])
 	        {
                   if (serial_read_gga(s_fd, gga, sizeof(gga)))
 	            {
+                      parse_gga(gga, &rtk_frame);
+
+                      if (serial_read_rmc(s_fd, rmc, sizeof(rmc)))
+	                {
+                          parse_rmc(rmc, &rtk_frame);
+			}
+		      printf("\nRTK DATA:\n");
+		      printf("timestamp: %d\n", rtk_frame.timestamp);
+		      printf("lat......: %d\n", rtk_frame.lat);
+		      printf("long.....: %d\n", rtk_frame.lon);
+		      printf("altitude.: %d\n", rtk_frame.alt);
+		      printf("velocity.: %d\n", rtk_frame.velocity);
+		      printf("heading..: %d\n", rtk_frame.heading);
+		      printf("fix......: %d\n", rtk_frame.fix);
+		      printf("id.......: %d\n", rtk_frame.id);
+
                       opmode = SX127X_OPMODE_TX;
                       ret = ioctl(fd, SX127XIOC_OPMODESET, (unsigned long)&opmode);
                       if (ret < 0)
@@ -256,6 +435,9 @@ int main(int argc, FAR char *argv[])
 	                  write(fd, p, FRAME_SIZE);
 
 	                  usleep(30000);
+
+			  p = &gga[95];
+			  memcpy(p, &rtk_frame, sizeof(rtk_frame));
 
 			  p = &gga[60];
 	                  write(fd, p, FRAME_SIZE);
